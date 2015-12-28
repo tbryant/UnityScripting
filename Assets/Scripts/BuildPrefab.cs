ce68657d5d909207e3ad2286b9ac64978298e4da
Tyler Bryant <tyler.bryant@gmail.com>
2015-12-28 11:29:32 -0800

    coroutine example

---
+++ Assets/Scripts/BuildPrefab.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildPrefab : MonoBehaviour {
+
+    public GameObject prefab;
+    public float delay = 1.0f;
+    
+    void Start() {
+        StartCoroutine(Build(delay));
+    }
+
+    IEnumerator Build(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Instantiate(prefab, transform.position, transform.rotation);
+    }
+    
+}
