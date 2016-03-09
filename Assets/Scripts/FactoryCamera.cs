fbdf03b6a5200297f38eaa875bada96c9e53ac97
Tyler Bryant <tyler.bryant@gmail.com>
2016-03-08 16:34:26 -0800

    Factory camera example

---
+++ Assets/Scripts/FactoryCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FactoryCamera : MonoBehaviour {
+
+    public GameObject cameraObject; //The camera that is in front of the ship.
+    public float distanceThreshold = 5.0f;
+
+    void Start ()
+    {
+
+        cameraObject.SetActive(false);
+
+    }
+    void Update () { 
+
+        // I would like to have in IF-statement where the ("Raycast")
+        // player is a certain distance from that TAG, then following this if statement.
+
+        float distanceToPlayer = (GameObject.FindGameObjectWithTag("Player").transform.position - transform.position).magnitude;
+
+        if (distanceToPlayer < distanceThreshold) {
+            cameraObject.SetActive (true);
+        }
+        else{
+            cameraObject.SetActive (false);
+        }
+
+    } 
+
+}
